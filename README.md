ab973d0b51c97468870d854351795eed8aa1f32c
Tom Shull <shull1@illinois.edu>
2015-04-10 15:56:54 -0500

    working on scripts to move everything in place automatically

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 This is all of my config files
 
 This is my record of them/one of the easier ways to transfer them between computers
+
+Scripts to use
---
+++ move_linux.sh
@@ -0,0 +1,9 @@
+
+rm ~/.bashrc
+ln -s /home/tshull226/Documents/SideHustle/config-files/bash/linux/.bashrc ~/.bashrc
+
+rm ~/.git-completion.bash
+ln -s /home/tshull226/Documents/SideHustle/config-files/git/.git-completion.bash ~/.git-completion.bash
+
+rm ~/.gitconfig
+ln -s /home/tshull226/Documents/SideHustle/config-files/git/.gitconfig ~/.gitconfig
